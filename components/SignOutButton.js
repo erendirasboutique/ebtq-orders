67effc855f20e5226a99315bae3b804ec78552f3
Dylan <hello@erendirasboutique.com>
2026-07-02 02:16:04 -0700

    Delete components/SignOutButton.js

--- components/SignOutButton.js
+++
@@ -1,8 +0,0 @@
-'use client';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-
-export default function SignOutButton(){
-  const supabase = createClientComponentClient();
-  async function signOut(){ await supabase.auth.signOut(); window.location.href='/login'; }
-  return <button className="pill" onClick={signOut}>Sign out</button>;
-}
